0d4285959cc10b79c81aba5812ad81fa51ba480c
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-19 18:13:18 -0400

    Update Main.php

--- src/LunarMoon72/CopySnail/Main.php
+++ src/LunarMoon72/CopySnail/Main.php
@@ -26,7 +26,7 @@ class Main extends PluginBase{
     }
     return true;
   }
-  public function ui($sender){
+  public function ui($player){
     $form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createCustomForm(function (Player $player, int $data = null){
       switch($data){
         case 0:
