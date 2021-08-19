0de11bc840ea6c50e9a66d82db3892f45dd1bbc9
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-19 17:21:42 -0400

    Update Main.php

--- src/LunarMoon72/CopySnail/Main.php
+++ src/LunarMoon72/CopySnail/Main.php
@@ -4,65 +4,57 @@ namespace LunarMoon72\CopySnail;
 
 use pocketmine\plugin\PluginBase;
 
-use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 
 use pocketmine\Player;
-use pocketmine \Server;
+use pocketmine\Server;
 
 class Main extends PluginBase
 {
-	public function onEnabled()
-	{
-		$this->getLogger()->info("Plugin Activated");
-	}
-	public function onDisabled()
-	{
-		$this->getLogger()->info("This is cool info");
-	}
-	public function onCommand(CommandSedner $sender, Command $cmd, String $label, Array $args) : bool
-	{
-		switch($cmd->getName())
-		{
-			case "uitest":
-			  if($sender instanceof Player)
-			  {
-			  	$this->ui($sender);
-			  }
-		}
-		return true;
-	}
-	public function ui($player)
-	{
-		$form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createSimpleForm(Player $player, int $data = null)
-		{
-			if($data === null)
-			{
-				return true;
-			}
-			switch($data())
-			{
-				case 0:
-				  $this->getServer()->dispatchCommand($player, "ce enchant driller 5");
+  public function onEnabled()
+  {
+    $this->getLogger()->info("Plugin Enabled");
+  }
+  public function onDisabled()
+  {
+    $this->getLogger()->info("Plugin off sadge");
+  }
+  public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+    switch($cmd->getName())
+    {
+      case "ceedit":
+        if($sender instanceof Player){
+          $this->ui($sender);
+        }
+    }
+    return true;
+    }
+  public function ui($player){
+    $form->getServer()->getPluginManager()->getPlugin("FormAPI")->createSimpleForm(function (Player $player, int $data = null){
+      if($data === null){
+        return true;
+      }
+      switch($data){
+        case 0:
+          $this->getServer()->dispatchCommand($player, "ce enchant driller" . $data[0]);
 
-				break;
+        break;
 
-				case 1:
-				  $this->getServer()->dispatchCommand($player, "ce enchant autorepair 5");
+        case 1:
+          $this->getServer()->dispatchCommand($player, "ce enchant driller" . $data[1]);
 
-				break;
+        break;
 
-				case 2:
-				  $this->getServer()->dispatchCommand($player, "ce enchant deathbringer 10");
-
-				break;
-			});
-            $form->setTitle("Admin Carnage UI");
-            $form->setContent("Choose a ce");
-            $form->addButton("Driller 5");
-            $form->addButton("Auto Repair 5");
-            $form->addButton("DeathBringer 10");
-		}
-
-	}
+        case 2:
+          $this->getServer()->dispatchCommand($player, "ce enchant driller" . $data[2]);
+      }
+      return true;
+    });
+    $form->setTitle("Choose an enchant");
+    $form->setContent("Choose an enchant and its level");
+    $form->addSlider("Driller", 0, 10);
+    $form->addSlider("Obsidian Sheild", 0, 10);
+    $form->addSlider("Auto Repair", 0, 10);
+  }
 }
