e31247a4d268701164ba09726ef92ca8bab22827
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-19 17:52:40 -0400

    Update Main.php

--- src/LunarMoon72/CopySnail/Main.php
+++ src/LunarMoon72/CopySnail/Main.php
@@ -10,31 +10,24 @@ use pocketmine\command\Command;
 use pocketmine\Player;
 use pocketmine\Server;
 
-class Main extends PluginBase
-{
-  public function onEnabled()
-  {
-    $this->getLogger()->info("Plugin Enabled");
+class Main extends PluginBase{
+  public function onEnabled(){
+    $this->getLogger()->info("Plugin is On");
   }
-  public function onDisabled()
-  {
-    $this->getLogger()->info("Plugin off sadge");
+  public function onDisabled(){
+    $this->getLogger()->info("Plugin OFF");
   }
   public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
-    switch($cmd->getName())
-    {
+    switch($cmd->getName()){
       case "ceedit":
         if($sender instanceof Player){
-          $this->ui($sender);
+          $sender->ui($player);
         }
     }
     return true;
-    }
+  }
   public function ui($player){
-    $form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createCustomForm(function (Player $player, int $data = null){
-      if($data === null){
-        return true;
-      }
+    $form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createCustomForm(function(Player $player, int $data = null){
       switch($data){
         case 0:
           $this->getServer()->dispatchCommand($player, "ce enchant driller" . $data[0]);
@@ -42,18 +35,13 @@ class Main extends PluginBase
         break;
 
         case 1:
-          $this->getServer()->dispatchCommand($player, "ce enchant driller" . $data[1]);
+          $this->getServer()->dispatchCommand($player, "ce enchant autorepair" . $data[0]);
 
         break;
-
-        case 2:
-          $this->getServer()->dispatchCommand($player, "ce enchant driller" . $data[2]);
       }
-      return true;
     });
-    $form->setTitle("Choose an enchant");
+    $form->addTitle("Select a CE");
     $form->addSlider("Driller", 0, 10);
-    $form->addSlider("Obsidian Sheild", 0, 10);
     $form->addSlider("Auto Repair", 0, 10);
   }
 }
